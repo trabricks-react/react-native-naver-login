443a4ec621c08ce4b9020b3edb67b01fecd5721d
Suhan Moon <lunyx@me.com>
2018-12-22 19:43:23 +0900

    Initalize

--- example/App.js
+++ example/App.js
@@ -7,43 +7,105 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import KakaoSDK from 'react-native-ccs-kakaosdk';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+	state = {
+
+	};
+
+	constructor(props) {
+		super(props);
+	}
+
+	login() {
+		this.setState({ loading: true });
+
+		KakaoSDK.login().then((response) => {
+			console.log(response);
+			this.setState({ loading: false });
+		}).catch(e => {
+			console.log(e);
+			this.setState({ loading: false });
+		});
+	}
+
+	async logout() {
+		this.setState({ loading: true });
+
+		KakaoSDK.logout().then((response) => {
+			console.log(response);
+			this.setState({ loading: false });
+		}).catch(e => {
+			console.log(e);
+			this.setState({ loading: false });
+		});
+	}
+
+	async getToken() {
+		this.setState({ loading: true });
+
+		KakaoSDK.getAccessToken().then(token => {
+			alert(JSON.stringify(token));
+			this.setState({ loading: false });
+		}).catch(e => {
+			console.log(e);
+			this.setState({ loading: false });
+
+		});
+	}
+		
+	render() {
+		return (
+			<View style={styles.container}>
+
+				{(this.state.loading && true) && (
+				<View style={ styles.loading }>
+					<ActivityIndicator />
+				</View>
+				)}
+
+				<TouchableOpacity onPress={() => this.login() } style={ styles.button }>
+					<Text>Login</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity onPress={() => this.logout() } style={ styles.button }>
+					<Text>Logout</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity onPress={() => this.getToken() } style={ styles.button }>
+					<Text>getToken</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+
+	loading : {
+		...StyleSheet.absoluteFill, 
+		zIndex: 10, 
+		backgroundColor: 'rgba(0,0,0,0.8)', 
+		alignItems: 'center', 
+		justifyContent: 'center'
+	},
+
+	button: {
+		borderColor: '#000', 
+		borderWidth: 1, 
+		padding: 10, 
+		width: 200, 
+		margin: 10,
+	}
 });
